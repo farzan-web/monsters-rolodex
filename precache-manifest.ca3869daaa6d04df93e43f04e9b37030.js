8d3a2340659199cded543a871ba513a730513c46
Farzan Moradi <farzan.moradi71@gmail.com>
2020-04-22 00:36:38 +0430

    Updates

---
+++ precache-manifest.ca3869daaa6d04df93e43f04e9b37030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bffe5b60247993fd2a51ead5327aaa4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0dc221ae28921531eeaf",
+    "url": "/monsters-rolodex/static/css/main.cd6d0cee.chunk.css"
+  },
+  {
+    "revision": "ad8534d43364924f4cac",
+    "url": "/monsters-rolodex/static/js/2.bedd2c39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bedd2c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc221ae28921531eeaf",
+    "url": "/monsters-rolodex/static/js/main.8c8860c1.chunk.js"
+  },
+  {
+    "revision": "52e9ddfea8938be86802",
+    "url": "/monsters-rolodex/static/js/runtime-main.e183efee.js"
+  }
+]);
